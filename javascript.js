fd7ddd846b33fb9153a973c1e1df01f687c8342e
Student <idk123@gmail.com>
2016-11-02 08:11:09 -1000

    new file:   javascript.js

---
+++ javascript.js
@@ -0,0 +1,52 @@
+/*block comment*/
+//comment
+
+//http://slides.com/ifang/javascriptbasics#/0/16 <---IMPORTANT
+
+//string
+var myName = "Jomari";
+//interger
+var myAge = 10;
+// boolean type (truth or false)
+var booleanExample = true;
+//unassigned
+undefined
+//Not a Number
+NaN
+//array
+
+//objects
+
+//functions is an execute statement
+function (){
+
+}
+//naming function
+function nameOfFunction (){
+console.log('hello me function')
+}
+//invoking function
+nameOfFunction();
+//parameters
+function hi (a, b) {
+	console.log(a + b);
+}
+
+hi(30,50)
+//return values
+function createGreeting (name,age){
+	if (age < 32){
+		console.log('hello ' + name);
+		return 'hello ' + name
+	}else{
+		console.log('hello Mr. ' + name);
+		return 'hello Mr. ' + name
+	}
+}
+createGreeting('Jomari', 16);
+//if lets all conditions be met (think like skyrim choices)
+// else if only lets one condition be met one at a time. (i.e. think mass effect choices)
+//= vs ==
+//= is an assignment
+//== checks for equality
+//== is a boolean operator
